e4f25236abb1e2d1097ace9e8936fe09300e84d8
AntonioFuziy <49486453+AntonioFuziy@users.noreply.github.com>
2019-04-17 08:32:41 -0300

    Adicionei nova feature: Sorteio

--- ep1.py
+++ ep1.py
@@ -3,7 +3,7 @@
 # Alunos: 
 # - aluno A: Fulano da Silva, fulanos@insper.edu.br
 # - aluno B: Sicrano de Almeida, sicranoa1@insper.edu.br
-
+import random
 def carregar_cenarios():
     cenarios = {
         "saguao": {
@@ -34,7 +34,9 @@ def carregar_cenarios():
         },
         "armario": {
             "titulo": " Voce chegou ao corredor dos armários",
-            "descricao": "",
+            "descricao": "Para adiar o EP você deve acertar esse enigma. "
+                         "Qual a resposta para a vida, o universo e tudo mais??? "
+                         "Se não souber a resposta volte à biblioteca e continue a sua jornada.",
             "opcoes": {
                 "biblioteca": "Voltar para a biblioteca",
             }
@@ -76,6 +78,7 @@ def main():
         print ()
 
         opcoes = cenario_atual['opcoes']
+
         if len(opcoes) == 0:
             print("Acabaram-se suas opções! Mwo mwo mwooooo...")
             game_over = True
@@ -85,16 +88,60 @@ def main():
             for k,v in opcoes.items():
                 print ('-{0}: {1}'.format(k,v))
             escolha =input("Digite a sua opção: ")
+            if escolha == 'biblioteca':
+                print(cenarios['biblioteca']['titulo'])
+                print('-'*len(cenarios['biblioteca']['titulo']))
+                print(cenarios['biblioteca']['descricao'])
+
+                #escolha = input ("Digite a sua opção: ")
+                print('''Aqui se encontra um teleporte que te levara aos armários,
+                        você têm duas opções:
+                        1°) A primeira é você participar de um jogo em que será 
+                        sorteado um número de 0 a 10, e você deve acerta-lo com
+                        apenas uma chance. Caso você não acerte, você será guiado
+                        a outra opção obrigatoriamente.
+                        2°) A segunda opção é você achar a chave secreta em algum
+                        local do Insper, essa chave chega abrirá o elevador. ''')
+
+                sorteio = input('''Pois então, vamos de sorteio ou prefere desistir
+                da tentativa ? (Sim/Não): ''')
+                print ( 'Se você recusar essa chance, você será teleportado para o saguão!!!')
+
+                if sorteio == 'Sim':
+                    numero_sorteio = random.randint(0, 1)
+                    numero_chute = int(input('Eai??? Qual o seu chute ?: '))
+
+                    if numero_chute == numero_sorteio:
+                        print ('Você ganhou e foi teleportado para a sala dos armarios')
+                        print ()
+                        cenario_atual="armario"
+                    else:
+                        print('''Você errou o número sorteado, e voltou para o saguão inicial''')
+
+
+                else:
+                    print('''Que pena, você desistiu do sorteio. Mas agora você têm
+                        uma nova missão para adiar seu EP, sem mais enrolações, agora
+                        ache a chave do elevador. Boa Sorte!!!''')
+
             while escolha not in opcoes:
                 if escolha!= opcoes:
                     escolha=input('Não existe essa opção. Escreva uma nova: ')
-            if escolha in opcoes:
-                nome_cenario_atual = escolha
-        
-                    
-                
+            if cenario_atual=='armario':
+                print(cenarios['armario']['titulo'])
+                print('-' * len(cenarios['armario']['titulo']))
+                print(cenarios['armario']['descricao'])
+                a=input('Qual a sua resposta: ')
+                print ()
+                if a=='42':
+                    game_over=True
+                else:
+                    print ('Você não acertou, e morreu. Volte para o saguão inicial e reinicie a sua jornada')
+                    print ()
 
-    print("Você morreu!")
+
+
+    print("Você ganhou!")
 
 #
 # Programa principal.
