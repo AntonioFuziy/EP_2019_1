7867fe95bfd063e28f07529bc6e2945f794421f5
AntonioFuziy <49486453+AntonioFuziy@users.noreply.github.com>
2019-04-18 21:46:49 -0300

    corrigi nova feature

--- pygame_musica.py
+++
@@ -1,5 +0,0 @@
-import pygame
-pygame.init()
-pygame.display.set_mode((200,100))
-pygame.mixer.music.load('Musica.mp3')
-pygame.mixer.music.play()
--- teste.py
+++
@@ -1,6 +0,0 @@
-import pygame
-pygame.init()
-pygame.display.set_mode((200,100))
-pygame.mixer.music.load('Musica.mp3')
-pygame.mixer.music.play(1)
-pygame.mixer.music.stop()
